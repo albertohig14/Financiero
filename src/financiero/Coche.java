185be8da2af4e521ca676fc53183cd1285d11301
usuario <usuario@172.16.2.178>
2019-03-22 18:03:09 +0100

    Version 1

--- src/financiero/Coche.java
+++ src/financiero/Coche.java
@@ -9,13 +9,6 @@ package financiero;
  *
  * @author usuario
  */
-public class Financiero {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Modificacion.");
-    }
+public class Coche {
     
 }
